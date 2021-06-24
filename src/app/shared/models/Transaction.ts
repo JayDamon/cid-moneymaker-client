c9086c1e214a61e86f5bea00f186979452434b47
jaydamon <Heron2128#>
2021-06-24 17:13:55 -0400

    update currency

--- src/app/shared/models/Transaction.ts
+++ src/app/shared/models/Transaction.ts
@@ -3,24 +3,6 @@ import { FinancialAccount } from './FinancialAccount';
 import { Category } from './Category';
 
 export interface Transaction {
-    // accountName: string;
-    // accountId: number;
-    // budgetName: string;
-    // budgetId: number;
-    // budgetCategoryId: number;
-    // budgetCategoryType: string;
-    // budgetCategoryTypeId: number;
-    // budgetCategoryName: string;
-    // budgetCategoryNameId: number;
-    // transactionCategory: string;
-    // transactionCategoryId: number;
-    // transactionTypeName: string;
-    // transactionTypeId: number;
-    // amount: number;
-    // description: string;
-    // date: Date;
-    // formattedDate: string;
-
     id: number;
     amount: number;
     description: string;
