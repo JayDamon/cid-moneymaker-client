bd1ddb3c8fc30064a1fe641bc5498ab81c412962
= <Jay.n.damon@gmail.com>
2021-08-23 09:55:48 -0400

    Add global table configuration

--- src/app/shared/components/transaction-table/transaction-table.component.ts
+++ src/app/shared/components/transaction-table/transaction-table.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, Input } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
+import { Component, ViewChild, Input, OnInit } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Transaction } from '../../models/Transaction';
 
@@ -22,6 +22,16 @@ export class TransactionTableComponent {
     @Input()
     set transactions(transactions: Array<Transaction>) {
       this._transactions = new MatTableDataSource(transactions);
+
+      this._transactions.sortingDataAccessor = (item, property) => {
+        switch(property) {
+          case 'accountName': return item.account.name;
+          case 'budgetName': return item.budget?.name;
+          case 'budgetCategoryName': return item.budget?.budgetCategory?.name;
+          default: return item[property];
+        }
+      }
+
       this._transactions.sort = this.sort;
     }
 
